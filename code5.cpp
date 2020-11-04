475ac1349c3b6b3b1fa166843b337715f947b15c
the-jay-gohil <73923650+the-jay-gohil@users.noreply.github.com>
2020-11-04 09:55:24 +0530

    Create code5.cpp

---
+++ code5.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+
+int main()
+{
+    Time time;
+     
+    time.getTime();
+    time.convertIntoSeconds();
+    time.displayTime();
+     
+    return 0;
+}
+ 
+class Time
+{
+    private:
+        int seconds;
+        int hh,mm,ss;
+    public:
+        void getTime(void);
+        void convertIntoSeconds(void);
+        void displayTime(void);
+};
+ 
+void Time::getTime(void)
+{
+    cout << "Enter time:" << endl;
+    cout << "Hours?   ";          cin >> hh;
+    cout << "Minutes? ";          cin >> mm;
+    cout << "Seconds? ";          cin >> ss;
+}
+ 
+void Time::convertIntoSeconds(void)
+{
+    seconds = hh*3600 + mm*60 + ss;
+}
+ 
+void Time::displayTime(void)
+{
+    cout << "The time is = " << setw(2) << setfill('0') << hh << ":"
+                             << setw(2) << setfill('0') << mm << ":"
+                             << setw(2) << setfill('0') << ss << endl;
+    cout << "Time in total seconds: " << seconds;
+}
